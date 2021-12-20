dd538c398e2d0d80c3cfe3cd19f843f78298f2b8
Hieu Man <hieuman2708@gmail.com>
2021-12-20 10:08:45 +0000

    -

--- experiments_bash.sh
+++ experiments_bash.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 echo "Nice to meet you"
 
-echo "\n MLE WITHOUT RECONSTRUCT TASK"
+# echo "\n MLE WITHOUT RECONSTRUCT TASK"
 # python run.py ESL -g 1 -mle -lr 0.006 -bs 32 -e 7 -w_gen 1.0
 
-echo "RL ONLY"
-python run.py ESL -g 1 -rl -l experiments/0.558-ESL-lr0.006-eps7-MLE1.0 -lr 0.0005 -bs 16 -e 5 -w_f1 0.5 -w_re 0.1 -w_mle 0.
+# echo "RL ONLY"
+# python run.py ESL -g 1 -rl -l experiments/0.558-ESL-lr0.006-eps7-MLE1.0 -lr 0.0005 -bs 16 -e 5 -w_f1 0.5 -w_re 0.1 -w_mle 0.
 
-echo "------------------------------------------------------------"
-echo "FULL WITHOUT RECONSTRUCT REWARD"
+# echo "------------------------------------------------------------"
+# echo "FULL WITHOUT RECONSTRUCT REWARD"
 # python run.py ESL -g 1 -mle -rl -l experiments/0.558-ESL-lr0.006-eps7-MLE1.0 -lr 0.0005 -bs 16 -e 5 -w_f1 0.5 -w_re 0. -w_mle 0.25
 
 echo "------------------------------------------------------------"
 echo "FULL WITHOUT GENERATE REWARD"
-python run.py ESL -g 1 -mle -rl -l experiments/0.558-ESL-lr0.006-eps7-MLE1.0 -lr 0.0005 -bs 16 -e 5 -w_f1 0.5 -w_re 0.5 -w_mle 0.25
+python run.py ESL -g 2 -mle -rl -l experiments/0.558-ESL-lr0.006-eps7-MLE1.0 -lr 0.0005 -bs 16 -e 5 -w_f1 0.5 -w_re 0.5 -w_mle 0.25
 
