e845950786545ac74075d1ee34ed6678bbf17bb9
Hieu Man <hieuman2708@gmail.com>
2021-12-22 02:43:10 +0000

    -

--- experiments_bash.sh
+++ experiments_bash.sh
@@ -11,7 +11,10 @@ echo "Nice to meet you"
 # echo "FULL WITHOUT RECONSTRUCT REWARD"
 # python run.py ESL -g 1 -mle -rl -l experiments/0.558-ESL-lr0.006-eps7-MLE1.0 -lr 0.0005 -bs 16 -e 5 -w_f1 0.5 -w_re 0. -w_mle 0.25
 
-echo "------------------------------------------------------------"
-echo "FULL WITHOUT GENERATE REWARD"
-python run.py ESL -g 2 -mle -rl -l experiments/0.558-ESL-lr0.006-eps7-MLE1.0 -lr 0.0005 -bs 16 -e 5 -w_f1 0.5 -w_re 0.5 -w_mle 0.25
+# echo "------------------------------------------------------------"
+# echo "FULL WITHOUT GENERATE REWARD"
+# python run.py ESL -g 2 -mle -rl -l experiments/0.558-ESL-lr0.006-eps7-MLE1.0 -lr 0.0005 -bs 16 -e 5 -w_f1 0.5 -w_re 0.5 -w_mle 0.25
 
+echo "------------------------------------------------------------"
+echo "FULL WITHOUT F1 REWARD"
+python run.py ESL -g 1 -mle -rl -l experiments/0.558-ESL-lr0.006-eps7-MLE1.0 -lr 0.0005 -bs 16 -e 5 -w_f1 0.0 -w_re 0.1 -w_mle 0.25
