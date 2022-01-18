c1fc82bfc4be19e48b2725af90efa4b1a63f2d24
Hieu Man <hieuman2708@gmail.com>
2022-01-18 03:04:50 +0000

    add: lastest_version

--- bash.sh
+++ bash.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Nice to meet you - Wish you have a great performance :))"
 python run.py Causal-TB -g 1 -mle -lr 0.006 -bs 32 -e 15 -w_gen 0.75
-python run.py Causal-TB -g 1 -mle -rl -l experiments/last_verson-Causal-TB-lr0.006-eps15-MLE1.0 -lr 0.0005 -bs 16 -e 15 -w_f1 0.5 -w_re 0.1 -w_mle 0.25
+python run.py Causal-TB -g 1 -mle -rl -l experiments/lastest_version-Causal-TB-lr0.006-eps15-MLE1.0 -lr 0.0005 -bs 16 -e 15 -w_f1 0.5 -w_re 0.1 -w_mle 0.1
